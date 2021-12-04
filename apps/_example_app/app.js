a15dffdbcf7dc046481b2f075c3a710cfee5e9d6
holkku <holkku@gmail.com>
2021-12-04 15:39:12 +0200

    Replace apps with BangleApps versions

--- apps/_example_app/app.js
+++ apps/_example_app/app.js
@@ -1,1 +1,12 @@
-// Your JS application in here
+// place your const, vars, functions or classes here
+
+// special function to handle display switch on
+Bangle.on('lcdPower', (on) => {
+  if (on) {
+    // call your app function here
+    // If you clear the screen, do Bangle.drawWidgets();
+  }
+});
+
+g.clear();
+// call your app function here
